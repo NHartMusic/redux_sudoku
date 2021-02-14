cb6719154e56100b13a32c9785db66296c552cdc
Nick Hart <nhartmusic@gmail.com>
2021-02-14 18:22:07 +0100

    set up theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {ThemeProvider} from 'styled-components'
 
 import { unregister } from './core'
-import { GlobalStyles } from './styles'
+import { GlobalStyles, theme } from './styles'
 
 ReactDOM.render(
   <React.StrictMode>
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <div>hello</div>
-    </>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
