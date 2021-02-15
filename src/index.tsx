851594d32d9e57044b2c877b72f4e8950c94b54b
Nick Hart <nhartmusic@gmail.com>
2021-02-15 13:56:49 +0100

    content styled component

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {ThemeProvider} from 'styled-components'
+import {Content} from './components'
 
 import { unregister } from './core'
 import { GlobalStyles, theme } from './styles'
@@ -9,7 +10,10 @@ ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
+      <Content>
       <div>hello</div>
+      </Content>
+      
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
