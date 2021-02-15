70fcd5fd91b4d8bcf900297233b4b3548c61f369
Nick Hart <nhartmusic@gmail.com>
2021-02-15 21:58:29 +0100

    fixed key error

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -3,13 +3,15 @@ import React, { FC } from 'react'
 const Grid: FC = () => {
     return (
         <div data-cy='grid-container'>
-            {[...Array(9)].map((_, rowIndex) => (
-            <div data-cy='grid-row-container'>
-                <div data-cy='block'></div>
-                <div data-cy='block'></div>
-                <div data-cy='block'></div>
+            {React.Children.toArray([...Array(9)].map((_, rowIndex) => (
+            <div data-cy='grid-row-container' key={rowIndex}>
+               {React.Children.toArray([...Array(9)].map((_, colIndex) => (
+                   <div data-cy="block"></div>
+               )))}
             </div>
-        ))}
+        )))}
         </div>
     )
 }
+
+export default Grid
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {ThemeProvider} from 'styled-components'
-import {Card, Content, Title} from './components'
+import {Card, Content, Grid, Title} from './components'
 
 import { unregister } from './core'
 import { GlobalStyles, theme } from './styles'
@@ -13,7 +13,7 @@ ReactDOM.render(
       <Content data-cy='content'>
       <Title data-cy='title'>Sudoku</Title>
       <Card data-cy='card'>
-        This is a card
+        <Grid />
       </Card>
       </Content>
       
