8a3cf75c33fd5c6102909c1916f214328a3ec05c
Nick Hart <nhartmusic@gmail.com>
2021-03-01 18:09:20 +0100

    setting up basic redux

---
+++ src/reducers/reducer.ts
@@ -0,0 +1,10 @@
+import { AnyAction } from 'redux'
+
+const initialState = {}
+
+function reducer(state = initialState, action: AnyAction) {
+    switch(action.type) {
+        default: 
+            return state
+    }
+}
