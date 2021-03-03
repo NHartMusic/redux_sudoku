de3995ef26028c792f6a181844c0fb7dff00d286
Nick Hart <nhartmusic@gmail.com>
2021-03-03 15:20:19 +0100

    mapping dat data

--- src/components/grid/block/index.tsx
+++ src/components/grid/block/index.tsx
@@ -1,4 +1,8 @@
 import React, {FC} from 'react'
+import { useSelector } from 'react-redux'
+
+import { IReducer } from 'reducers'
+import { N } from 'typings'
 
 import { Container } from './styles'
 
@@ -7,10 +11,18 @@ interface IPROPS {
     rowIndex: number
 }
 
+interface IState {
+    value: N
+}
+
 const Block: FC<IPROPS> = ({ colIndex, rowIndex}) => {
-    return (
-        <Container data-cy={`block-${rowIndex}-${colIndex}`}>0</Container> 
-    )
+    const state = useSelector<IReducer, IState>(({ grid }) => ({ 
+        value: grid ? grid[rowIndex][colIndex] : 0
+    }))
+
+    return <Container data-cy={`block-${rowIndex}-${colIndex}`}>
+        {state.value === 0 ? '' : state.value}
+    </Container> 
 }
 
 export default Block
