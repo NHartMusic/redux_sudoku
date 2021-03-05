657badb2a7863b81e9e0bcb18da99e4d0317e6ca
Nick Hart <nhartmusic@gmail.com>
2021-03-05 17:50:07 +0100

    setting up remove numbers function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,8 @@
 export { default as checkGrid } from './check-grid'
 export { default as createFullGrid } from './create-full-grid'
 export { default as fillGrid } from './fill-grid'
+export { default as getRandomIndex } from './get-random-index'
 export { default as identifySquare } from './identify-square'
+export { default as removeNumbers } from './remove-numbers'
 export {default as shuffle} from './shuffle'
 export * from './is-in'
