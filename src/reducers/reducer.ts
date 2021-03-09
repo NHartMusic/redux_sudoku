45336efe7d09965c065cc4527fced8e68820a03f
Nick Hart <nhartmusic@gmail.com>
2021-03-09 16:19:11 +0100

    working on challenge grid function

--- src/reducers/reducer.ts
+++ src/reducers/reducer.ts
@@ -1,6 +1,6 @@
 import { AnyAction } from 'redux'
 
-import { createFullGrid } from 'utils'
+import { copyGrid, createFullGrid, removeNumbers } from 'utils'
 
 import { IReducer } from './interfaces'
 import * as types from './types'
@@ -10,9 +10,12 @@ const initialState: IReducer = {}
 function reducer(state = initialState, action: AnyAction): IReducer {
     switch(action.type) {
         case types.CREATE_GRID: 
+            const solvedGrid = createFullGrid()
+            const gridCopy = copyGrid(solvedGrid)
+            const challengeGrid = removeNumbers(gridCopy)
             return {
                 ...state,
-                grid: createFullGrid()
+                grid: challengeGrid
             }
 
         case types.SELECT_BLOCK:
--- src/utils/solve-grid/index.ts
+++ src/utils/solve-grid/index.ts
@@ -37,6 +37,7 @@ function solveGrid(grid: GRID) {
                         }
                     }
             }
+            break
         }
     }
 
